3483e530258f1710108d27312658d9f149c7234f
edgar6bf <edgar6bf@gmail.com>
2023-11-12 00:10:41 +0900

    test: 최소 주문 금액 검증 테스트와 프로모션 유효 날짜 검증 테스트 분리
    최소 주문 금액 검증과 프로모션 유효 날짜 검증은 모든 프로모션의 공통 테스트므로 중복 로직을 줄이기 위해 별도의 PromotionTest로 분리하였다.

--- src/test/java/christmas/domain/promotion/ChristmasDdayDiscountPromotionTest.java
+++ src/test/java/christmas/domain/promotion/ChristmasDdayDiscountPromotionTest.java
@@ -1,11 +1,9 @@
 package christmas.domain.promotion;
 
-import christmas.constant.Giveaway;
 import christmas.domain.order.OrderDate;
 import christmas.domain.order.OrderMenu;
 import christmas.domain.order.OrderMenus;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,8 +11,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static christmas.constant.Giveaway.*;
-import static org.assertj.core.api.Assertions.*;
+import static christmas.constant.Giveaway.NO_GIVEAWAY;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ChristmasDdayDiscountPromotionTest {
@@ -52,39 +50,5 @@ class ChristmasDdayDiscountPromotionTest {
                 arguments(24, 3300),
                 arguments(25, 3400)
         );
-    }
-
-    @DisplayName("이벤트 기간이 아닌 주문 날짜를 입력하면 할인 혜택을 받을 수 없다.")
-    @Test
-    void applyPromotionWithNotPromotionDate() throws Exception {
-        // Given
-        OrderDate orderDate = new OrderDate(27);
-        OrderMenus orderMenus = new OrderMenus(List.of(new OrderMenu("타파스", 4)));
-        int expectedDiscountPrice = 0;
-        Promotion promotion = new ChristmasDdayDiscountPromotion();
-
-        // When
-        PromotionBenefits promotionBenefits = promotion.applyPromotion(orderDate, orderMenus);
-
-        // Then
-        assertThat(promotionBenefits.getDiscountPrice()).isEqualTo(expectedDiscountPrice);
-        assertThat(promotionBenefits.getGiveaway()).isEqualTo(NO_GIVEAWAY);
-    }
-
-    @DisplayName("10000원 미만의 주문 메뉴 목록을 입력하면 할인 혜택을 받을 수 없다.")
-    @Test
-    void applyPromotionWithUnderMinimumTotalPrice() throws Exception {
-        // Given
-        OrderDate orderDate = new OrderDate(22);
-        OrderMenus orderMenus = new OrderMenus(List.of(new OrderMenu("타파스", 1)));
-        int expectedDiscountPrice = 0;
-        Promotion promotion = new ChristmasDdayDiscountPromotion();
-
-        // When
-        PromotionBenefits promotionBenefits = promotion.applyPromotion(orderDate, orderMenus);
-
-        // Then
-        assertThat(promotionBenefits.getDiscountPrice()).isEqualTo(expectedDiscountPrice);
-        assertThat(promotionBenefits.getGiveaway()).isEqualTo(NO_GIVEAWAY);
     }
 }
---
+++ src/test/java/christmas/domain/promotion/PromotionTest.java
@@ -0,0 +1,66 @@
+package christmas.domain.promotion;
+
+import christmas.domain.order.OrderDate;
+import christmas.domain.order.OrderMenu;
+import christmas.domain.order.OrderMenus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static christmas.constant.PromotionTitle.CHRISTMAS_DDAY_DISCOUNT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class PromotionTest {
+
+    @DisplayName("10000원 미만의 주문 메뉴 목록을 입력하면 할인 혜택을 받을 수 없다.")
+    @MethodSource("promotionsAndValidOrderDates")
+    @ParameterizedTest(name = "[{index}] {2}")
+    void applyPromotionWithUnderMinimumTotalPrice(Promotion promotion, OrderDate orderDate, String promotionTitle) throws Exception {
+        // Given
+        OrderMenus orderMenusHasSmallTotalOrderPrice = new OrderMenus(
+                List.of(new OrderMenu("타파스", 1))
+        );
+
+        // When
+        PromotionBenefits promotionBenefits = promotion.applyPromotion(orderDate, orderMenusHasSmallTotalOrderPrice);
+
+        // Then
+        assertThat(promotionBenefits.hasBenefits()).isFalse();
+    }
+
+    private static Stream<Arguments> promotionsAndValidOrderDates() {
+        return Stream.of(
+                arguments(new ChristmasDdayDiscountPromotion(), new OrderDate(23), CHRISTMAS_DDAY_DISCOUNT.getTitle())
+        );
+    }
+
+    @DisplayName("이벤트 기간에 포함되지 않는 주문 날짜를 입력하면 할인 혜택을 받을 수 없다.")
+    @MethodSource("promotionsAndInvalidOrderDates")
+    @ParameterizedTest(name = "[{index}] {2}")
+    void applyPromotionWithNotPromotionDate(Promotion promotion, OrderDate orderDate, String promotionTitle) throws Exception {
+        // Given
+        OrderMenus orderMenus = new OrderMenus(
+                List.of(
+                        new OrderMenu("티본스테이크", 10),
+                        new OrderMenu("초코케이크", 5)
+                )
+        );
+
+        // When
+        PromotionBenefits promotionBenefits = promotion.applyPromotion(orderDate, orderMenus);
+
+        // Then
+        assertThat(promotionBenefits.hasBenefits()).isFalse();
+    }
+
+    private static Stream<Arguments> promotionsAndInvalidOrderDates() {
+        return Stream.of(
+                arguments(new ChristmasDdayDiscountPromotion(), new OrderDate(28), CHRISTMAS_DDAY_DISCOUNT.getTitle())
+        );
+    }
+}
